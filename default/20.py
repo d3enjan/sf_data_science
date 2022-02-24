d5079630a8464a739e36ae154ffcfef9e2e83d4a
d3enjan <d3en@mail.ru>
2022-02-24 16:57:48 +0300

    new cod

--- default/20.py
+++ default/20.py
@@ -1,27 +1,64 @@
-def optimal_predict(number: int = 1) -> int:
-    '''Игра компьютер угадает число меньше чем за 20 попыток'''
+"""Игра угадай число
+Компьютер сам загадывает и сам угадывает число
+"""
 
-    import numpy as np
+import numpy as np
 
+
+def random_predict(number: int = 1) -> int:
+    """Рандомно угадываем число
+
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    count = 0
+    
     min = 1
     max = 101
-
-    number = np.random.randint(min, max)
-
-    count = 0
-
-    while True:
-        count+=1
-        mid = (min+max) // 2
     
-        if mid > number:
+    predict_number = np.random.randint(min, max)  # предполагаемое число
+    
+    while True:
+        count += 1
+        mid = (min+max) // 2
+        
+        if mid > predict_number:
             max = mid
     
-        elif mid < number:
+        elif mid < predict_number:
             min = mid
 
         else:
-            print(f"Компьютер угадал число за {count} попыток. Это число {number}")
+            print(f"Компьютер угадал число за {count} попыток. Это число {predict_number}")
             break #конец игры выход из цикла
     return count
-optimal_predict()
+    
+
+
+def score_game(random_predict) -> int:
+    """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
+
+    Args:
+        random_predict ([type]): функция угадывания
+
+    Returns:
+        int: среднее количество попыток
+    """
+    count_ls = []
+    #np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
+
+    for number in random_array:
+        count_ls.append(random_predict(number))
+
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
+    return score
+
+
+if __name__ == "__main__":
+    # RUN
+    score_game(random_predict)
